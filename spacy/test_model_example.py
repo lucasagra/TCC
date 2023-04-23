44445d53618e2dc8b690752798ba3c431c9b53b7
lucas <lucasomena@hotmail.com>
2023-04-23 16:54:04 -0300

    Updating spacy config epochs and testing script.

--- spacy/test_model_example.py
+++ spacy/test_model_example.py
@@ -1,10 +1,9 @@
 import spacy
 import argparse
 
-def test_model(model: str):
+def test_model(model: str, report:str):
     print("Loading model ", model, "...\n")
     nlp = spacy.load(model)
-    report = "Nódulo não calcificado, contornos regulares, no segmento anterior do lobo superior esquerdo, medindo 0,3 cm, indeterminado."
     doc = nlp(report)
 
     print("Evaluating text:")
@@ -13,13 +12,14 @@ def test_model(model: str):
     print("Entities found:")
     for ent in doc.ents:
         text = "'" + ent.text + "'"
-        print(f"{ent.label_:<15}: {text:<35} ({ent.start_char}, {ent.end_char}) ")
+        print(f"{ent.label_:<15}: {text:<45} ({ent.start_char}, {ent.end_char}) ")
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Test a NER model')
     parser.add_argument('--model', help="Model's path", default='./output/model-best')
+    parser.add_argument('--report', help="Report to be evaluated", default='Nódulo não calcificado, contornos regulares, no segmento anterior do lobo superior esquerdo, medindo 0,3 cm, indeterminado.')
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parse_args()
-    test_model(args.model)
+    test_model(args.model, args.report)
